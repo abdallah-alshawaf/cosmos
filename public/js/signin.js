b213306c022eb738c7a1ffabe0ed120ac9844edf
shathakh <shathaqudaih17@gmail.com>
2022-08-31 11:55:57 +0300

    create sign in and sign up pages and make client side validation relates #7 relates #8

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,10 +1,73 @@
-const signinButton = document.querySelector('.signin-button');
+//client side validation
+const form= document.querySelector('.form')
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
-signinButton.addEventListener('click', (e) => {
+
+// form.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+
+//     ValidateInputs()
+// })
+
+const setError=(element, msg, index2)=>{
+    const formControl = element.parentElement;
+    const small = document.querySelectorAll(".small");
+    small.forEach((e, index) => {
+        if (index === index2) {
+        e.classList.add('error')
+        e.innerText = msg;
+    }
+    })
+    formControl.classList.add('error')
+    formControl.classList.remove('success')
+}
+
+const setSuccess=(element, msg)=>{
+    const formControl = element.parentElement;
+    const small = document.querySelector(".small");
+    small.innerText = '';
+
+    formControl.classList.add('success')
+    formControl.classList.remove('error')
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const ValidateInputs=()=>{
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    
+    if(emailValue===''){
+        setError(email, 'email is required', 0)
+    }else if(!isValidEmail(emailValue)){
+        setError(email, 'provide a valid email value', 0)
+    }else{
+        setSuccess(email)
+    }
+
+    if(passwordValue===''){
+        setError(password, 'password is required', 1)
+    }else if(passwordValue.length<8){
+        setError(password, 'Password must be at least 8 character', 1)
+    }else{
+        setSuccess(password)
+    }
+
+    
+}
+//end client side validation
+const signInButton = document.querySelector('.signin-button');
+
+signInButton.addEventListener('click', (e) => {
     e.preventDefault();
+    ValidateInputs()
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
 
-    const email = document.querySelector('.email').value;
-    const password = document.querySelector('.password').value;
 
     fetch('/signin', {
         method: 'POST', 
@@ -17,7 +80,7 @@ signinButton.addEventListener('click', (e) => {
     }) 
     .then(response => response.json())
     .then(res => console.log(res))
-    .then(window.location.href = '/html/signup.html')
+    // .then(window.location='/')
     .catch(err => console.log(err))
 
 })
