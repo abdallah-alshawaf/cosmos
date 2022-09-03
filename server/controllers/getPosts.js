c3abe1892eb274ba3fb2632fc5da25902cd47e31
abdallah-alshawaf <abdallah.i.alshawaf@gmail.com>
2022-09-03 10:57:48 +0300

    error handling

--- server/controllers/getPosts.js
+++ server/controllers/getPosts.js
@@ -4,8 +4,15 @@ const getPosts=(req,res)=>{
       getPostsQuery().then(data=> res.json(data.rows))
 }
 
-const getOnePost = (req,res)=>{
-      getOnePostQuery(req.params.id).then(data=> res.json(data.rows))
+const getOnePost = (req, res)=>{
+      getOnePostQuery(req.params.id)
+      .then(data=> res.json(data.rows))
+      .catch(err => { 
+            console.log(err);
+            res.json('Error Not Found');
+      }
+
+      )
 }
 
 module.exports= {getOnePost, getPosts}
